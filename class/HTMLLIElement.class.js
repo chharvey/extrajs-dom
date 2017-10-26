bf7adb9288ddd139e02740759a413b65743c78b4
Chris Harvey <chrisharvey2pi@gmail.com>
2017-10-26 17:04:45 -0400

    fix subclassing

--- class/HTMLLIElement.class.js
+++ class/HTMLLIElement.class.js
@@ -1,11 +1,11 @@
-const Element = require('../src/Element.class.js')
+const HTMLElement = require('./HTMLElement.class.js')
 
 /**
  * Represents an HTML LI element.
  * @see https://www.w3.org/TR/html/grouping-content.html#htmllielement-htmllielement
- * @extends Element
+ * @extends HTMLElement
  */
-class HTMLLIElement extends Element {
+class HTMLLIElement extends HTMLElement {
   /**
    * @summary Construct a new HTMLLIElement object.
    * @version EXPERIMENTAL
--- class/HTMLUListElement.class.js
+++ class/HTMLUListElement.class.js
@@ -1,11 +1,11 @@
-const Element = require('../src/Element.class.js')
+const HTMLElement = require('./HTMLElement.class.js')
 
 /**
  * Represents an HTML UL element.
  * @see https://www.w3.org/TR/html/grouping-content.html#htmlulistelement-htmlulistelement
- * @extends Element
+ * @extends HTMLElement
  */
-class HTMLUListElement extends Element {
+class HTMLUListElement extends HTMLElement {
   /**
    * @summary Construct a new HTMLUListElement object.
    * @version EXPERIMENTAL
