7b652707cfcab31378f067fc3de7a6d83c734586
Chris Harvey <chrisharvey2pi@gmail.com>
2017-12-19 20:31:32 -0500

    start HTMLHyperlinkElementUtils implementation

--- class/HTMLAnchorElement.class.js
+++ class/HTMLAnchorElement.class.js
@@ -41,7 +41,7 @@ class HTMLAnchorElement extends HTMLElement {
    */
   _reinitializeURL() {
     let non_relative_flag_set = true // TODO
-    if (this._url && this._url.protocol.slice(0,4) === 'blob' && non_relative_flag_set) return;
+    if (this._url && this._url.protocol === 'blob:' && non_relative_flag_set) return;
     else this._setTheURL()
   }
 
@@ -60,6 +60,124 @@ class HTMLAnchorElement extends HTMLElement {
       else return this._url
     }
   }
+
+  /**
+   * @summary Returns the hyperlink’s URL’s origin.
+   * @see https://www.w3.org/TR/html/links.html#dom-htmlhyperlinkelementutils-origin
+   * @type {string}
+   */
+  get origin() {
+    this._reinitializeURL()
+    return (this._url === null) ? '' : this._url.origin
+  }
+
+  /**
+   * @summary Returns the hyperlink’s URL’s scheme.
+   * @see https://www.w3.org/TR/html/links.html#dom-htmlhyperlinkelementutils-protocol
+   * @type {[type]}
+   */
+  get protocol() {}
+  /**
+   * @summary Can be set, to change the URL’s scheme.
+   * @param {[type]} [description]
+   */
+  set protocol() {}
+
+  /**
+   * @summary Returns the hyperlink’s URL’s username.
+   * @see https://www.w3.org/TR/html/links.html#dom-htmlhyperlinkelementutils-username
+   * @type {[type]}
+   */
+  get username() {}
+  /**
+   * @summary Can be set, to change the URL’s username.
+   * @param {[type]} [description]
+   */
+  set username() {}
+
+  /**
+   * @summary Returns the hyperlink’s URL’s password.
+   * @see https://www.w3.org/TR/html/links.html#dom-htmlhyperlinkelementutils-password
+   * @type {[type]}
+   */
+  get password() {}
+  /**
+   * @summary Can be set, to change the URL’s password.
+   * @param {[type]} [description]
+   */
+  set password() {}
+
+  /**
+   * @summary Returns the hyperlink’s URL’s host and port (if different from the default port for the scheme).
+   * @see https://www.w3.org/TR/html/links.html#dom-htmlhyperlinkelementutils-host
+   * @type {[type]}
+   */
+  get host() {}
+  /**
+   * @summary Can be set, to change the URL’s host and port.
+   * @param {[type]} [description]
+   */
+  set host() {}
+
+  /**
+   * @summary Returns the hyperlink’s URL’s host.
+   * @see https://www.w3.org/TR/html/links.html#dom-htmlhyperlinkelementutils-hostname
+   * @type {[type]}
+   */
+  get hostname() {}
+  /**
+   * @summary Can be set, to change the URL’s host.
+   * @param {[type]} [description]
+   */
+  set hostname() {}
+
+  /**
+   * @summary Returns the hyperlink’s URL’s port.
+   * @see https://www.w3.org/TR/html/links.html#dom-htmlhyperlinkelementutils-port
+   * @type {[type]}
+   */
+  get port() {}
+  /**
+   * @summary Can be set, to change the URL’s port.
+   * @param {[type]} [description]
+   */
+  set port() {}
+
+  /**
+   * @summary Returns the hyperlink’s URL’s path.
+   * @see https://www.w3.org/TR/html/links.html#dom-htmlhyperlinkelementutils-pathname
+   * @type {[type]}
+   */
+  get pathname() {}
+  /**
+   * @summary Can be set, to change the URL’s path.
+   * @param {[type]} [description]
+   */
+  set pathname() {}
+
+  /**
+   * @summary Returns the hyperlink’s URL’s query (includes leading "?" if non-empty).
+   * @see https://www.w3.org/TR/html/links.html#dom-htmlhyperlinkelementutils-search
+   * @type {[type]}
+   */
+  get search() {}
+  /**
+   * @summary Can be set, to change the URL’s query (ignores leading "?").
+   * @param {[type]} [description]
+   */
+  set search() {}
+
+  /**
+   * @summary Returns the hyperlink’s URL’s fragment (includes leading "#" if non-empty).
+   * @see https://www.w3.org/TR/html/links.html#dom-htmlhyperlinkelementutils-hash
+   * @type {[type]}
+   */
+  get hash() {}
+  /**
+   * @summary Can be set, to change the URL’s fragment (ignores leading "#").
+   * @param {[type]} [description]
+   */
+  set hash() {}
 }
 
 module.exports = HTMLAnchorElement
