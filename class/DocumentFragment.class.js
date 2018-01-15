50da5d2a1a2808c7624645ca2ae0922b7f17add7
Chris Harvey <chrisharvey2pi@gmail.com>
2018-01-15 15:27:55 -0500

    add DocumentFragment.innerHTML

--- class/DocumentFragment.class.js
+++ class/DocumentFragment.class.js
@@ -45,6 +45,27 @@ class DocumentFragment {
       (c instanceof Element) ? c.view.html() : c
     ).join('')
   }
+
+  /**
+   * @summary Return the "innerHTML" of a document fragment.
+   * @version EXPERIMENTAL
+   * @param   {DocumentFragment} frag the document fragment to stringify
+   * @returns {string} a concatenation of the `outerHTML` of the fragment’s element children
+   */
+  static innerHTML(frag) {
+    return Array.from(frag.childNodes).map(function (node) {
+      // https://developer.mozilla.org/en-US/docs/Web/API/Node/nodeType
+      let mapfn = {
+        1: (el)   => el.outerHTML    , // ELEMENT_NODE
+        2: (node) => null            , // ATTRIBUTE_NODE
+        3: (text) => text.textContent, // TEXT_NODE
+        8: (comm) => null            , // COMMENT_NODE
+        11: (frag) => DocumentFragment.innerHTML(frag), // DOCUMENT_FRAGMENT_NODE
+        default: (node) => null,
+      }
+      return (mapfn[node.nodeType] || mapfn.default)(node)
+    }).join('')
+  }
 }
 
 module.exports = DocumentFragment
