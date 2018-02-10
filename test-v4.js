d928efdf4abaa127baccf4182d0d0a72c0f255a0
Chris Harvey <chrisharvey2pi@gmail.com>
2018-02-10 18:29:45 -0500

    add test-v4

---
+++ test-v4.js
@@ -0,0 +1,29 @@
+const jsdom = require('jsdom')
+
+const xjs = {
+  ...require('extrajs'),
+  ...require('./index.js')
+}
+
+let document = new jsdom.JSDOM(`
+<!doctype html>
+<html lang="en">
+<head><title>test</title></head>
+<body>
+<p>Hello world.</p>
+<h1>Pets:</h1>
+<ul>
+  <li>Bird</li>
+  <li>Cat</li>
+  <li>Dog</li>
+</ul>
+</body>
+</html>
+`).window.document
+
+
+let list = document.querySelector('ul')
+let xlist = new xjs.HTMLUListElement(list).class('testing123')
+
+
+console.log(xlist.outerHTML())
