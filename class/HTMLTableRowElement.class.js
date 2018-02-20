fa8fea84d9aa3d7175043a079b8eaa05633820ed
Chris Harvey <chrisharvey2pi@gmail.com>
2018-02-20 14:09:00 -0500

    create TableSection & TableRow & populate methods

---
+++ class/HTMLTableRowElement.class.js
@@ -0,0 +1,84 @@
+const xjs = {
+  HTMLElement: require('./HTMLElement.class.js'),
+}
+
+/**
+ * Wrapper for HTML `tr` element.
+ * @see https://www.w3.org/TR/html/tabular-data.html#htmltablerowelement
+ * @extends xjs.HTMLElement
+ */
+xjs.HTMLTableRowElement = class extends xjs.HTMLElement {
+  /**
+   * @summary Construct a new xjs.HTMLTableRowElement object.
+   * @version EXPERIMENTAL
+   * @param {HTMLTableRowElement} node the node to wrap
+   */
+  constructor(node) {
+    super(node)
+  }
+  /**
+   * @summary This wrapper’s node.
+   * @type {HTMLTableRowElement}
+   */
+  get node() { return super.node }
+
+  /**
+   * @summary Populate this list with items containing data.
+   * @description This method appends items to the end of this list.
+   * The items are the result of rendering the given data.
+   * In order to determine how the data is rendered, this `<tr>` element must have
+   * a `<template>` child, which in turn has a single child that is an `<td>`.
+   *
+   * Notes:
+   * - This element may contain multiple `<template>` children, but this method uses only the first one.
+   * - This element may also already have any number of `<td>` children; they are not affected.
+   *
+   * @example
+   * let {document} = new jsdom.JSDOM(`
+   * <table>
+   *   <tbody>
+   *     <tr>
+   *       <template>
+   *         <td>
+   *           <a href="{{ url }}">{{ text }}</a>
+   *         </td>
+   *       </template>
+   *     </tr>
+   *   </tbody>
+   * <table>
+   * `).window
+   * let data = [
+   *   { "url": "#0", "text": "Career Connections" },
+   *   { "url": "#1", "text": "Getting Licensed & Certified" },
+   *   { "url": "#2", "text": "Career resources" },
+   *   { "url": "#3", "text": "Code of Ethics" }
+   * ]
+   * new xjs.HTMLDListElement(document.querySelector('tr'))
+   *   .populate(data, function (f, d) {
+   *     f.querySelector('a').href        = d.url
+   *     f.querySelector('a').textContent = d.text
+   *   })
+   *
+   * @param   {Array} data any array of things
+   * @param   {xjs.HTMLTemplateElement~RenderingFunction=} renderer a typical rendering function
+   * @throws  {ReferenceError} if this `<tr>` does not contain a `<template>`,
+   *                           or if that `<template>` does not contain exactly 1 `<td>`.
+   * @returns {xjs.HTMLListElement} `this`
+   */
+  populate(data, renderer = (f,d) => {}) {
+    let template = this.node.querySelector('template')
+    if (template===null) {
+      throw new ReferenceError('This <tr> does not have a <template> descendant.')
+    }
+    if (template.content.children.length !== 1 || !template.content.children[0].matches('td')) {
+      throw new ReferenceError('The <template> must contain exactly 1 element, which must be a <td>.')
+    }
+    let component = new xjs.HTMLTemplateElement(template).setRenderer(renderer)
+    return this.append(
+      new xjs.DocumentFragment(jsdom.JSDOM.fragment(''))
+        .append(...data.map((datum) => component.render(datum)))
+    )
+  }
+}
+
+module.exports = xjs.HTMLTableSectionElement
---
+++ class/HTMLTableSectionElement.class.js
@@ -0,0 +1,82 @@
+const xjs = {
+  HTMLElement: require('./HTMLElement.class.js'),
+}
+
+/**
+ * Wrapper for HTML `thead`, `tfoot`, and `tbody` elements.
+ * @see https://www.w3.org/TR/html/tabular-data.html#htmltablesectionelement
+ * @extends xjs.HTMLElement
+ */
+xjs.HTMLTableSectionElement = class extends xjs.HTMLElement {
+  /**
+   * @summary Construct a new xjs.HTMLTableSectionElement object.
+   * @version EXPERIMENTAL
+   * @param {HTMLTableSectionElement} node the node to wrap
+   */
+  constructor(node) {
+    super(node)
+  }
+  /**
+   * @summary This wrapper’s node.
+   * @type {HTMLTableSectionElement}
+   */
+  get node() { return super.node }
+
+  /**
+   * @summary Populate this list with items containing data.
+   * @description This method appends items to the end of this list.
+   * The items are the result of rendering the given data.
+   * In order to determine how the data is rendered, this `<thead/tfoot/tbody>` element must have
+   * a `<template>` child, which in turn has a single child that is an `<tr>`.
+   *
+   * Notes:
+   * - This element may contain multiple `<template>` children, but this method uses only the first one.
+   * - This element may also already have any number of `<tr>` children; they are not affected.
+   *
+   * @example
+   * let {document} = new jsdom.JSDOM(`
+   * <table>
+   *   <tbody>
+   *     <template>
+   *       <tr>
+   *         <td>{{ url }}</td><td>{{ text }}</td>
+   *       </tr>
+   *     </template>
+   *   </tbody>
+   * <table>
+   * `).window
+   * let data = [
+   *   { "url": "#0", "text": "Career Connections" },
+   *   { "url": "#1", "text": "Getting Licensed & Certified" },
+   *   { "url": "#2", "text": "Career resources" },
+   *   { "url": "#3", "text": "Code of Ethics" }
+   * ]
+   * new xjs.HTMLDListElement(document.querySelector('tbody'))
+   *   .populate(data, function (f, d) {
+   *     f.querySelectorAll('td')[0].textContent = d.url
+   *     f.querySelectorAll('td')[1].textContent = d.text
+   *   })
+   *
+   * @param   {Array} data any array of things
+   * @param   {xjs.HTMLTemplateElement~RenderingFunction=} renderer a typical rendering function
+   * @throws  {ReferenceError} if this `<thead/tfoot/tbody>` does not contain a `<template>`,
+   *                           or if that `<template>` does not contain exactly 1 `<tr>`.
+   * @returns {xjs.HTMLListElement} `this`
+   */
+  populate(data, renderer = (f,d) => {}) {
+    let template = this.node.querySelector('template')
+    if (template===null) {
+      throw new ReferenceError('This <thead/tfoot/tbody> does not have a <template> descendant.')
+    }
+    if (template.content.children.length !== 1 || !template.content.children[0].matches('tr')) {
+      throw new ReferenceError('The <template> must contain exactly 1 element, which must be a <tr>.')
+    }
+    let component = new xjs.HTMLTemplateElement(template).setRenderer(renderer)
+    return this.append(
+      new xjs.DocumentFragment(jsdom.JSDOM.fragment(''))
+        .append(...data.map((datum) => component.render(datum)))
+    )
+  }
+}
+
+module.exports = xjs.HTMLTableSectionElement
