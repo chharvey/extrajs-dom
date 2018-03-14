6f8819cea18ddc14debb96bcb218d45c3592de3c
Chris Harvey <chrisharvey2pi@gmail.com>
2018-03-13 22:54:22 -0400

    make xjs.DocumentFragment#importLinks

--- class/DocumentFragment.class.js
+++ class/DocumentFragment.class.js
@@ -1,9 +1,13 @@
+const fs = require('fs')
+const path = require('path')
+
 const jsdom = require('jsdom')
 
 const xjs = {
   Object : require('extrajs').Object,
   Node   : require('./Node.class.js'),
   Element: require('./Element.class.js'),
+  HTMLTemplateElement: require('./HTMLTemplateElement.class.js'),
 }
 
 /**
@@ -107,6 +111,71 @@ xjs.DocumentFragment = class extends xjs.Node {
       (c instanceof xjs.Node) ? c.node :
       (c === null) ? '' : c
     ))
+    return this
+  }
+
+
+  /**
+   * @summary Replace all `link[rel="import"][data-import]` elements with contents from their documents.
+   * @description
+   * This method finds all `link[rel="import"][data-import]`s in this document fragment,
+   * and then replaces those links with another `DocumentFragment` holding some contents.
+   * These contents depend on the value set for `data-import`:
+   *
+   * - if `[data-import="document"]`, then the replaced contents will be the contents of the link’s imported document itself
+   * - if `[data-import="template"]`, then the replaced contents will be the contents of the first `template` descendant in the link’s imported document
+   * - if the `[data-import]` attribute value is neither `"document"` nor `"template"`, or if it is absent, then the `link` element is completely ignored and left as-is
+   *
+   * Note: If {@link https://developer.mozilla.org/en-US/docs/Web/API/HTMLLinkElement/import|HTMLLinkElement#import}
+   * is supported (by the browser or jsdom), then when `[data-import="document"]` is set,
+   * the appended contents will instead be a `Document` object, as defined by
+   * {@link https://www.w3.org/TR/html-imports/|HTML Imports}, rather than a `DocumentFragment` object.
+   *
+   * In the example below,
+   * The `link[rel="import"]` in this fragment has `[data-import="template"]`, and so is replaced with
+   * the contents of `template#item-template` in `x-linked-doc.tpl.html`---namely,
+   * a `DocumentFragment` containing the `section` element.
+   * However, if `[data-import="document"]`, then the replaced content would consist of
+   * a `DocumentFragment` (or a `Document`) containing the entirety of `x-linked-doc.tpl.html`,
+   * including both the `template#item-template` along with the `h2`.
+   *
+   * @example
+   * this.innerHTML() === `
+   *   <ol>
+   *     <template id="list-template">
+   *       <li>
+   *         <link rel="import" href="../tpl/x-linked-doc.tpl.html" data-import="template"/>
+   *       </li>
+   *     </template>
+   *   </ol>
+   * `
+   *
+   * // x-linked-doc.tpl.html:
+   * <template id="item-template">
+   *   <section>
+   *     <h1>a hed</h1>
+   *     <p>a graf</p>
+   *   </section>
+   * </template>
+   * <h2>another hed</h2>
+   *
+   * @param   {string} relativepath should always be `__dirname` when called
+   * @returns {xjs.DocumentFragment} `this`
+   */
+  importLinks(relativepath) {
+    this.node.querySelectorAll('link[rel="import"][data-import]').forEach(function (link) {
+      // if `HTMLLinkElement#import` is not yet supported, `'import' in link` will be false
+      const import_switch = {
+        'document': () => ('import' in link) ? link.import.cloneNode(true)                                   : jsdom.JSDOM.fragment(fs.readFileSync(path.resolve(relativepath, link.href), 'utf8')),
+        'template': () => ('import' in link) ? link.import.querySelector('template').content.cloneNode(true) : xjs.HTMLTemplateElement.fromFileSync(path.resolve(relativepath, link.href)).content(),
+        default() { return null },
+      }
+      let imported = (import_switch[link.getAttribute('data-import')] || import_switch.default).call(null)
+      if (imported && link.parentElement) {
+        link.parentElement.append(imported)
+        link.remove() // link.href = path.resolve('https://example.com/index.html', link.href) // TODO set the href relative to the current window.location.href
+      }
+    })
     return this
   }
 
