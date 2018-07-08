4de215fcafed3795b0aec0b80d9401fff8072d0b
Chris Harvey <chrisharvey2pi@gmail.com>
2018-07-07 22:10:55 -0400

    set up typescript gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,16 @@
 const gulp  = require('gulp')
 const jsdoc = require('gulp-jsdoc3')
+const typescript = require('gulp-typescript')
+// require('typescript') // DO NOT REMOVE … peerDependency of `gulp-typescript`
 
+const tsconfig      = require('./config/tsconfig.json')
+
+
+gulp.task('compile', async function () {
+  return gulp.src('./src/class/*.ts')
+    .pipe(typescript(tsconfig.compilerOptions))
+    .pipe(gulp.dest('./dist/class/'))
+})
 
 // HOW-TO: https://github.com/mlucool/gulp-jsdoc3#usage
 gulp.task('docs:api', function () {
@@ -8,7 +18,7 @@ gulp.task('docs:api', function () {
     .pipe(jsdoc(require('./config/jsdoc.json')))
 })
 
-gulp.task('build', ['docs:api'])
+gulp.task('build', ['compile', 'docs:api'])
 
 gulp.task('test', function () {
   require('./test/DocumentFragment-importLinks.test.js');
