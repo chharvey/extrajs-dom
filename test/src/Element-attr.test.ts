3d1a314644a3f299cbbe939b48cf0de8d2b50568
Chris Harvey <chrisharvey2pi@gmail.com>
2018-09-09 00:12:14 -0400

    simplify test string templates

--- test/src/Element-attr.test.ts
+++ test/src/Element-attr.test.ts
@@ -10,34 +10,34 @@ let x: xjs.Element = new xjs.Element(jsdom.JSDOM.fragment('<span></span>').query
 export default Promise.all([
 	test(x.outerHTML(), '<span></span>')
 		// set an attribute to a string
-		.then(() => test(`${x.attr('attr1', 'val1').outerHTML()}`, '<span attr1="val1"></span>'))
-		.then(() => test(`${x.attr('attr1')}`                    , 'val1'))
+		.then(() => test(x.attr('attr1', 'val1').outerHTML(), '<span attr1="val1"></span>'))
+		.then(() => test(x.attr('attr1') !                  , 'val1'))
 		// add a boolean attribute
-		.then(() => test(`${x.attr('attr2', '').outerHTML()}`    , '<span attr1="val1" attr2=""></span>'))
-		.then(() => test(`${x.attr('attr2')}`                    , ''))
+		.then(() => test(x.attr('attr2', '').outerHTML()    , '<span attr1="val1" attr2=""></span>'))
+		.then(() => test(x.attr('attr2') !                  , ''))
 		// remove an attribute
-		.then(() => test(`${x.attr('attr2', null).outerHTML()}`  , '<span attr1="val1"></span>'))
-		.then(() => test(`${x.attr('attr2')}`                    , 'null'))
+		.then(() => test(x.attr('attr2', null).outerHTML()  , '<span attr1="val1"></span>'))
+		.then(() => test(x.attr('attr2') !                  , 'null'))
 		// set an attribute to the string `'null'`
-		.then(() => test(`${x.attr('attr2', 'null').outerHTML()}`, '<span attr1="val1" attr2="null"></span>'))
-		.then(() => test(`${x.attr('attr2')}`                    , 'null'))
+		.then(() => test(x.attr('attr2', 'null').outerHTML(), '<span attr1="val1" attr2="null"></span>'))
+		.then(() => test(x.attr('attr2') !                  , 'null'))
 		// set an attribute using a function
-		.then(() => test(`${x.attr('attr3', function () { return this.attr('attr1') }).outerHTML()}`, '<span attr1="val1" attr2="null" attr3="val1"></span>'))
+		.then(() => test(x.attr('attr3', function () { return this.attr('attr1') }).outerHTML(), '<span attr1="val1" attr2="null" attr3="val1"></span>'))
 		.then(() => test((() => {
 			let valueFn: ValueFunction = function (this: xjs.Element) { return this.attr('attr2') }
-			return `${x.attr('attr3', valueFn).outerHTML()}`
+			return x.attr('attr3', valueFn).outerHTML()
 		})(), '<span attr1="val1" attr2="null" attr3="null"></span>'))
 		// call `attr()` with an object
-		.then(() => test(`${x.attr({ attr1: 'string', attr2: 42, attr3: true }).outerHTML()}`       , '<span attr1="string" attr2="42" attr3="true"></span>'))
-		.then(() => test(`${x.attr({ attr1: null }).outerHTML()}`                                   , '<span attr2="42" attr3="true"></span>'))
+		.then(() => test(x.attr({ attr1: 'string', attr2: 42, attr3: true }).outerHTML(), '<span attr1="string" attr2="42" attr3="true"></span>'))
+		.then(() => test(x.attr({ attr1: null }).outerHTML()                            , '<span attr2="42" attr3="true"></span>'))
 		// call `attr()` with no args, `null`, or an empty object `{}`
-		.then(() => test(`${x.attr(    ).outerHTML()}`                                              , '<span attr2="42" attr3="true"></span>'))
-		.then(() => test(`${x.attr(null).outerHTML()}`                                              , '<span attr2="42" attr3="true"></span>'))
-		.then(() => test(`${x.attr({}  ).outerHTML()}`                                              , '<span attr2="42" attr3="true"></span>'))
+		.then(() => test(x.attr(    ).outerHTML()                                       , '<span attr2="42" attr3="true"></span>'))
+		.then(() => test(x.attr(null).outerHTML()                                       , '<span attr2="42" attr3="true"></span>'))
+		.then(() => test(x.attr({}  ).outerHTML()                                       , '<span attr2="42" attr3="true"></span>'))
 		// fail to call `attr()` with `''`
 		.then(() => test((() => {
 			try {
-				return x.attr('')
+				return x.attr('') !
 			} catch (e) {
 				return e.name
 			}
--- test/src/HTMLElement-style.test.ts
+++ test/src/HTMLElement-style.test.ts
@@ -10,37 +10,37 @@ let x: xjs.HTMLElement = new xjs.HTMLElement(jsdom.JSDOM.fragment('<span></span>
 export default Promise.all([
 	test(x.outerHTML(), '<span></span>')
 		// set a property to a string
-		.then(() => test(`${x.style('background', 'none').outerHTML()}` , '<span style="background: none;"></span>'))
-		.then(() => test(`${x.style('background')}`                     , 'none'))
-		.then(() => test(`${x.style('font-weight', 'bold').outerHTML()}`, '<span style="background: none; font-weight: bold;"></span>'))
+		.then(() => test(x.style('background', 'none').outerHTML() , '<span style="background: none;"></span>'))
+		.then(() => test(x.style('background') !                   , 'none'))
+		.then(() => test(x.style('font-weight', 'bold').outerHTML(), '<span style="background: none; font-weight: bold;"></span>'))
 		// remove a property using `''`
-		.then(() => test(`${x.style('font-weight', '').outerHTML()}`    , '<span style="background: none;"></span>'))
-		.then(() => test(`${x.style('font-weight')}`                    , 'null'))
+		.then(() => test(x.style('font-weight', '').outerHTML()    , '<span style="background: none;"></span>'))
+		.then(() => test(x.style('font-weight') !                  , 'null'))
 		// remove a property using `null`
-		.then(() => test(`${x.style('background', null).outerHTML()}`   , '<span style=""></span>'))
-		.then(() => test(`${x.style('background')}`                     , 'null'))
+		.then(() => test(x.style('background', null).outerHTML()   , '<span style=""></span>'))
+		.then(() => test(x.style('background') !                   , 'null'))
 		// set a property using a function
-		.then(() => test(`${x.style('content', function () { return this.tagName }).outerHTML()}`       , '<span style="content: span;"></span>'))
-		.then(() => test(`${x.style('content', function () { return `'${this.tagName}'` }).outerHTML()}`, '<span style="content: \'span\';"></span>'))
+		.then(() => test(x.style('content', function () { return this.tagName }).outerHTML()       , '<span style="content: span;"></span>'))
+		.then(() => test(x.style('content', function () { return `'${this.tagName}'` }).outerHTML(), '<span style="content: \'span\';"></span>'))
 		.then(() => test((() => {
 			let valueFn: ValueFunction = function (this: xjs.HTMLElement) { return null }
-			return `${x.style('content', valueFn).outerHTML()}`
+			return x.style('content', valueFn).outerHTML()
 		})(), '<span style=""></span>'))
 		// call `style()` with an object
-		.then(() => test(`${x.style({
+		.then(() => test(x.style({
 			content: null,
 			background: 'transparent',
 			'font-weight': 400,
 			'flex-wrap': true,
-		}).outerHTML()}`, '<span style="background: transparent; font-weight: 400; flex-wrap: true;"></span>'))
+		}).outerHTML(), '<span style="background: transparent; font-weight: 400; flex-wrap: true;"></span>'))
 		// call `style()` with no args, `null`, or an empty object `{}`
-		.then(() => test(`${x.style(    ).outerHTML()}`, '<span style="background: transparent; font-weight: 400; flex-wrap: true;"></span>'))
-		.then(() => test(`${x.style(null).outerHTML()}`, '<span style="background: transparent; font-weight: 400; flex-wrap: true;"></span>'))
-		.then(() => test(`${x.attr({}  ).outerHTML()}` , '<span style="background: transparent; font-weight: 400; flex-wrap: true;"></span>'))
+		.then(() => test(x.style(    ).outerHTML(), '<span style="background: transparent; font-weight: 400; flex-wrap: true;"></span>'))
+		.then(() => test(x.style(null).outerHTML(), '<span style="background: transparent; font-weight: 400; flex-wrap: true;"></span>'))
+		.then(() => test(x.attr({}   ).outerHTML(), '<span style="background: transparent; font-weight: 400; flex-wrap: true;"></span>'))
 		// fail to call `style()` with `''`
 		.then(() => test((() => {
 			try {
-				return x.style('')
+				return x.style('') !
 			} catch (e) {
 				return e.name
 			}
