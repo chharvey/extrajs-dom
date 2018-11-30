e78d966b8c5d779da6e6c3a660c9aaf235943c94
Chris Harvey <chrisharvey2pi@gmail.com>
2018-11-30 17:23:05 -0500

    add methods xjs.HTMLTableElement#{caption,tHead,tFoot}

--- src/class/HTMLTableElement.class.ts
+++ src/class/HTMLTableElement.class.ts
@@ -1,6 +1,7 @@
 import {Processor, ProcessingFunction} from 'template-processor'
 
 import xjs_HTMLElement from './HTMLElement.class'
+import xjs_HTMLTableSectionElement from './HTMLTableSectionElement.class'
 
 
 /**
@@ -19,6 +20,60 @@ export default class xjs_HTMLTableElement extends xjs_HTMLElement {
 	 * This wrapper’s node.
 	 */
 	get node(): HTMLTableElement { return super.node as HTMLTableElement }
+
+	/**
+	 * Get this table’s `<caption>` element, if it has one, otherwise return `null`.
+	 * @see https://www.w3.org/TR/html52/tabular-data.html#dom-htmltableelement-caption
+	 * @returns this table’s `<caption>` child, or `null` if it does not exist
+	 */
+	caption(): HTMLTableCaptionElement|null;
+	/**
+	 * Set or remove this table’s `<caption>` element.
+	 * @param   caption the `<caption>` to set, or `null` to remove it
+	 * @returns `this`
+	 */
+	caption(caption: HTMLTableCaptionElement|null): this;
+	caption(caption?: any): any {
+		if (arguments.length === 0) return this.node.caption
+		this.node.caption = caption
+		return this
+	}
+
+	/**
+	 * Get this table’s `<thead>` element, if it has one, otherwise return `null`.
+	 * @see https://www.w3.org/TR/html52/tabular-data.html#dom-htmltableelement-thead
+	 * @returns this table’s `<thead>` child, or `null` if it does not exist
+	 */
+	tHead(): HTMLTableSectionElement|null;
+	/**
+	 * Set or remove this table’s `<thead>` element.
+	 * @param   header the `<thead>` to set, or `null` to remove it
+	 * @returns `this`
+	 */
+	tHead(header: xjs_HTMLTableSectionElement|HTMLTableSectionElement|null): this;
+	tHead(header?: any): any {
+		if (arguments.length === 0) return this.node.tHead
+		this.node.tHead = (header instanceof xjs_HTMLTableSectionElement) ? header.node : header
+		return this
+	}
+
+	/**
+	 * Get this table’s `<tfoot>` element, if it has one, otherwise return `null`.
+	 * @see https://www.w3.org/TR/html52/tabular-data.html#dom-htmltableelement-tfoot
+	 * @returns this table’s `<tfoot>` child, or `null` if it does not exist
+	 */
+	tFoot(): HTMLTableSectionElement|null;
+	/**
+	 * Set or remove this table’s `<tfoot>` element.
+	 * @param   footer the `<tfoot>` to set, or `null` to remove it
+	 * @returns `this`
+	 */
+	tFoot(footer: xjs_HTMLTableSectionElement|HTMLTableSectionElement|null): this;
+	tFoot(footer?: any): any {
+		if (arguments.length === 0) return this.node.tFoot
+		this.node.tFoot = (footer instanceof xjs_HTMLTableSectionElement) ? footer.node : footer
+		return this
+	}
 
 	/**
 	 * Populate this list with items containing data.
