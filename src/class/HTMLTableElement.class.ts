19eb6aafea0bcb70a7a15ad7169f4783476c9fcc
Chris Harvey <chrisharvey2pi@gmail.com>
2019-02-02 16:04:33 -0500

    method #populate throws TypeError

--- src/class/HTMLTableElement.class.ts
+++ src/class/HTMLTableElement.class.ts
@@ -111,16 +111,16 @@ export default class xjs_HTMLTableElement extends xjs_HTMLElement {
 	 * @param   options additional processing options for all items
 	 * @param   this_arg provide a `this` context to the processing function
 	 * @returns `this`
-	 * @throws  {ReferenceError} if this `<table>` does not contain a `<template>`,
+	 * @throws  {TypeError} if this `<table>` does not contain a `<template>`,
 	 *                           or if that `<template>` does not contain exactly 1 `<tbody>`.
 	 */
 	populate<T, U extends object>(instructions: ProcessingFunction<T, U>, dataset: T[], options?: U, this_arg: unknown = this): this {
 		let template: HTMLTemplateElement|null = this.node.querySelector('template')
 		if (template === null) {
-			throw new ReferenceError('This <table> does not have a <template> descendant.')
+			throw new TypeError('This <table> does not have a <template> descendant.')
 		}
 		if (template.content.children.length !== 1 || !template.content.children[0].matches('tbody')) {
-			throw new ReferenceError('The <template> must contain exactly 1 element, which must be a <tbody>.')
+			throw new TypeError('The <template> must contain exactly 1 element, which must be a <tbody>.')
 		}
 		let processor: Processor<T, U> = new Processor(template, instructions)
 		return this.append(...dataset.map((data) => processor.process(data, options, this_arg)))
