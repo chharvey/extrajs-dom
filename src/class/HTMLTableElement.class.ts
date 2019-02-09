1cdee69ffa0c90e3e25ca57306cecd9d77671778
Chris Harvey <chrisharvey2pi@gmail.com>
2019-02-09 01:47:02 -0500

    instance methods `#populate` use `Processor.populateList`

--- src/class/HTMLTableElement.class.ts
+++ src/class/HTMLTableElement.class.ts
@@ -1,4 +1,4 @@
-import {Processor, ProcessingFunction} from 'template-processor'
+import { Processor, ProcessingFunction } from 'template-processor'
 
 import xjs_HTMLElement from './HTMLElement.class'
 import xjs_HTMLTableSectionElement from './HTMLTableSectionElement.class'
@@ -78,51 +78,17 @@ export default class xjs_HTMLTableElement extends xjs_HTMLElement {
 	/**
 	 * Populate this list with items containing data.
 	 *
-	 * This method appends items to the end of this list.
-	 * The items are the result of rendering the given data.
-	 * In order to determine how the data is rendered, this `<table>` element must have
-	 * a `<template>` child, which in turn has a single child that is a `<tbody>`.
-	 *
-	 * Notes:
-	 * - This element may contain multiple `<template>` children, but this method uses only the first one.
-	 * - This element may also already have any number of `<tbody>` children; they are not affected.
-	 *
-	 * ```js
-	 * let {document} = new jsdom.JSDOM(`
-	 * <table>
-	 * 	<template>
-	 * 		<tbody>...</tbody>
-	 * 	</template>
-	 * <table>
-	 * `).window
-	 * let data = [...]
-	 * new xjs_HTMLTableElement(document.querySelector('table'))
-	 * 	.populate(function (f, d, o) { ... }, data)
-	 * new xjs_HTMLTableElement(document.querySelector('table'))
-	 * 	.populate(function (f, d, o) {
-	 * 		// some code involving `this`
-	 * 	}, data, {}, other_context)
-	 * ```
-	 *
-	 * @param   <T> the type of the data to fill
-	 * @param   <U> the type of the `options` object
-	 * @param   instructions a typical {@link ProcessingFunction} to modify the template
-	 * @param   dataset the data to populate the list
-	 * @param   options additional processing options for all items
-	 * @param   this_arg provide a `this` context to the processing function
+	 * Call {@link Processor.populateList} on this list.
+	 * @param   <T>          the type of the data to fill
+	 * @param   <U>          the type of the `options` object
+	 * @param   instructions the processing function to use
+	 * @param   dataset      the data to populate this list
+	 * @param   options      additional processing options for all items
+	 * @param   this_arg     the `this` context, if any, in which the instructions is called
 	 * @returns `this`
-	 * @throws  {TypeError} if this `<table>` does not contain a `<template>`,
-	 *                           or if that `<template>` does not contain exactly 1 `<tbody>`.
 	 */
 	populate<T, U extends object>(instructions: ProcessingFunction<T, U>, dataset: T[], options?: U, this_arg: unknown = this): this {
-		let template: HTMLTemplateElement|null = this.node.querySelector('template')
-		if (template === null) {
-			throw new TypeError('This <table> does not have a <template> descendant.')
-		}
-		if (template.content.children.length !== 1 || !template.content.children[0].matches('tbody')) {
-			throw new TypeError('The <template> must contain exactly 1 element, which must be a <tbody>.')
-		}
-		let processor: Processor<T, U> = new Processor(template, instructions)
-		return this.append(...dataset.map((data) => processor.process(data, options, this_arg)))
+		Processor.populateList(this.node, instructions, dataset, options, this_arg)
+		return this
 	}
 }
--- src/class/HTMLTableSectionElement.class.ts
+++ src/class/HTMLTableSectionElement.class.ts
@@ -1,4 +1,4 @@
-import {Processor, ProcessingFunction} from 'template-processor'
+import { Processor, ProcessingFunction } from 'template-processor'
 
 import xjs_HTMLElement from './HTMLElement.class'
 
@@ -20,66 +20,20 @@ export default class xjs_HTMLTableSectionElement extends xjs_HTMLElement {
    */
   get node(): HTMLTableSectionElement { return super.node as HTMLTableSectionElement }
 
-  /**
-   * Populate this list with items containing data.
-   *
-   * This method appends items to the end of this list.
-   * The items are the result of rendering the given data.
-   * In order to determine how the data is rendered, this `<thead/tfoot/tbody>` element must have
-   * a `<template>` child, which in turn has a single child that is a `<tr>`.
-   *
-   * Notes:
-   * - This element may contain multiple `<template>` children, but this method uses only the first one.
-   * - This element may also already have any number of `<tr>` children; they are not affected.
-   *
-   * ```js
-   * let {document} = new jsdom.JSDOM(`
-   * <table>
-   *   <tbody>
-   *     <template>
-   *       <tr>
-   *         <td>{{ url }}</td><td>{{ text }}</td>
-   *       </tr>
-   *     </template>
-   *   </tbody>
-   * <table>
-   * `).window
-   * let data = [
-   *   { "url": "#0", "text": "Career Connections" },
-   *   { "url": "#1", "text": "Getting Licensed & Certified" },
-   *   { "url": "#2", "text": "Career resources" },
-   *   { "url": "#3", "text": "Code of Ethics" }
-   * ]
-   * new xjs_HTMLTableSectionElement(document.querySelector('tbody'))
-   *   .populate(function (f, d, o) {
-   *     f.querySelectorAll('td')[0].textContent = d.url
-   *     f.querySelectorAll('td')[1].textContent = d.text
-   *   }, data)
-   * new xjs_HTMLTableSectionElement(document.querySelector('tbody'))
-   *  .populate(function (f, d, o) {
-   *    // some code involving `this`
-   *  }, data, {}, other_context)
-   * ```
-   *
-   * @param   <T> the type of the data to fill
-   * @param   <U> the type of the `options` object
-   * @param   instructions a typical {@link ProcessingFunction} to modify the template
-   * @param   dataset the data to populate the list
-   * @param   options additional processing options for all items
-   * @param   this_arg provide a `this` context to the processing function
-   * @returns `this`
-   * @throws  {TypeError} if this `<thead/tfoot/tbody>` does not contain a `<template>`,
-   *                           or if that `<template>` does not contain exactly 1 `<tr>`.
-   */
-  populate<T, U extends object>(instructions: ProcessingFunction<T, U>, dataset: T[], options?: U, this_arg: unknown = this): this {
-    let template: HTMLTemplateElement|null = this.node.querySelector('template')
-    if (template === null) {
-      throw new TypeError('This <thead/tfoot/tbody> does not have a <template> descendant.')
-    }
-    if (template.content.children.length !== 1 || !template.content.children[0].matches('tr')) {
-      throw new TypeError('The <template> must contain exactly 1 element, which must be a <tr>.')
-    }
-    let processor: Processor<T, U> = new Processor(template, instructions)
-    return this.append(...dataset.map((data) => processor.process(data, options, this_arg)))
-  }
+	/**
+	 * Populate this list with items containing data.
+	 *
+	 * Call {@link Processor.populateList} on this list.
+	 * @param   <T>          the type of the data to fill
+	 * @param   <U>          the type of the `options` object
+	 * @param   instructions the processing function to use
+	 * @param   dataset      the data to populate this list
+	 * @param   options      additional processing options for all items
+	 * @param   this_arg     the `this` context, if any, in which the instructions is called
+	 * @returns `this`
+	 */
+	populate<T, U extends object>(instructions: ProcessingFunction<T, U>, dataset: T[], options?: U, this_arg: unknown = this): this {
+		Processor.populateList(this.node, instructions, dataset, options, this_arg)
+		return this
+	}
 }
